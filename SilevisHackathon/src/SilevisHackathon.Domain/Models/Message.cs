bdeed2ae93ba815c7be7d078e69238d40ebb8220
Michał Szymczyk <93574747+Zimeek@users.noreply.github.com>
2022-12-02 19:23:48 +0100

    update message model

--- SilevisHackathon/src/SilevisHackathon.Domain/Models/Message.cs
+++ SilevisHackathon/src/SilevisHackathon.Domain/Models/Message.cs
@@ -5,5 +5,7 @@ public class Message
     public int Id { get; set; }
     public string Content { get; set; }
     public int PersonId { get; set; }
+    public int? TeamId { get; set; }
+    public int? EventId { get; set; }
     public DateTime Date { get; set; }
 }
