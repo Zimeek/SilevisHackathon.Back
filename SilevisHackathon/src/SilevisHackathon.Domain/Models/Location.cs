9b1cf5cd657aad1fbb62a820418672c401e5a3e8
Michał Szymczyk <93574747+Zimeek@users.noreply.github.com>
2022-12-02 19:15:55 +0100

    update domain models

--- SilevisHackathon/src/SilevisHackathon.Domain/Models/Location.cs
+++ SilevisHackathon/src/SilevisHackathon.Domain/Models/Location.cs
@@ -2,5 +2,7 @@ namespace SilevisHackathon.Domain.Models;
 
 public class Location
 {
-    
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Address { get; set; }
 }
--- SilevisHackathon/src/SilevisHackathon.Domain/Models/Person.cs
+++ SilevisHackathon/src/SilevisHackathon.Domain/Models/Person.cs
@@ -2,5 +2,10 @@ namespace SilevisHackathon.Domain.Models;
 
 public class Person
 {
-    
+    public int Id { get; set; }
+    public string NickName { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string TeamId { get; set; }
+
 }
