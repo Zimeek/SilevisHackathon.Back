f035b27abbb9583e122ac34f6372c07057f55bd7
Michał Szymczyk <93574747+Zimeek@users.noreply.github.com>
2022-12-03 12:39:53 +0100

    add new query

---
+++ SilevisHackathon/src/SilevisHackathon.Application/Queries/GetAllTeamsQuery.cs
@@ -0,0 +1,28 @@
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using SilevisHackathon.Domain.Models;
+using SilevisHackathon.Infrastructure.Data;
+
+namespace SilevisHackathon.Application.Queries;
+
+public class GetAllTeamsQuery
+{
+    public record Query() : IRequest<ICollection<Team>>;
+
+    public class Handler : IRequestHandler<Query, ICollection<Team>>
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public Handler(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<ICollection<Team>> Handle(Query request, CancellationToken cancellationToken)
+        {
+            return await _dbContext.Teams
+                .AsNoTracking()
+                .ToListAsync(cancellationToken);
+        }
+    }
+}
